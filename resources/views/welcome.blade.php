36888acfd0569c305e79a0c7345f69ad88817ca8
Ricardo RPM <ricardomayorga0709@gmail.com>
2025-09-13 06:26:48 -0500

    trabajo en inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -341,6 +341,12 @@
         </section>
     </main>
 
+    
+    
+    
+    
+    
+
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
