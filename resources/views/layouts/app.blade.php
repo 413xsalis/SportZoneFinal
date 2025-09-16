ff3fc0e548c31873840efd7a0a5c0b33b69332be
alex sanchez <alexsanchez.az2005@gmail.com>
2025-09-16 15:07:49 -0500

    actualizacion de logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="{{ asset('logo.png') }}" type="image/png">
+    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
 
 </head>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,7 @@
     <title>SPORT_ZONE</title>
     <!-- Bootstrap CSS -->
     <!-- Favicon -->
-    <link rel="icon" href="{{ asset('logo.png') }}" type="image/png">
-    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+ 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
