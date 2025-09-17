6fbb662f4ad4e4d4a74dc6afc5d5f44e66a19a74
alex sanchez <alexsanchez.az2005@gmail.com>
2025-09-16 22:43:27 -0500

    Update trashed.blade.php

--- resources/views/administrador/Gestion_usuarios/trashed.blade.php
+++ resources/views/administrador/Gestion_usuarios/trashed.blade.php
@@ -57,6 +57,9 @@
             </tbody>
         </table>
     </div>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 @endsection
